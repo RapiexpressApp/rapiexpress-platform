81a207dcc0b48025a8705beb085a8689ff06f5ab
radriann21 <adrianrangelargu@gmail.com>
2026-09-06 20:59:23 -0400

    fix: repair shared config packages and align portal tooling

--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -1,12 +1,11 @@
+import prettierConfig from "eslint-config-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 import baseConfig from "./base.js";
 
 export default [
   ...baseConfig,
-  {
-    files: ["**/*.{ts,tsx,jsx}"],
-    rules: {
-      "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",
-    },
-  },
+  reactHooks.configs.flat.recommended,
+  reactRefresh.configs.vite,
+  prettierConfig,
 ];
